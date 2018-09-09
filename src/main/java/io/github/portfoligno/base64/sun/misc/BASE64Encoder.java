630048971852dd733f333751690108febf9c293b
Nicholas Yip <yiplunkwun@gmail.com>
2018-09-10 06:14:57 +0900

    Account for differences in the treatments of line endings

--- src/main/java/io/github/portfoligno/base64/sun/misc/BASE64Encoder.java
+++ src/main/java/io/github/portfoligno/base64/sun/misc/BASE64Encoder.java
@@ -2,13 +2,20 @@ package io.github.portfoligno.base64.sun.misc;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class BASE64Encoder extends CharacterEncoder {
-  private final @NotNull Base64.Encoder delegate = Base64.getMimeEncoder();
+  private static final @NotNull String LINE_SEPARATOR = System.lineSeparator();
+
+  private final @NotNull Base64.Encoder delegate = LINE_SEPARATOR.equals("\r\n") ?
+      Base64.getMimeEncoder() :
+      Base64.getMimeEncoder(76, LINE_SEPARATOR.getBytes(StandardCharsets.US_ASCII));
 
   @Override
   public @NotNull String encode(@NotNull byte[] aBuffer) {
-    return delegate.encodeToString(aBuffer);
+    String s = delegate.encodeToString(aBuffer);
+    int n = s.length();
+    return n % (76 + LINE_SEPARATOR.length()) == 76 && s.charAt(n - 1) != '=' ? s + LINE_SEPARATOR : s;
   }
 }
